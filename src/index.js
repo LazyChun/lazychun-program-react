0a2672976ea93bfa9bb8c68289bc04b4745eae8b
LazyChun <1299362885@qq.com>
2021-02-24 17:10:49 +0800

    调整React脚手架模板

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App.js";
 import config from "./lazychun";
-import { getConfig } from "../libs/config";
-import serverStore from "../libs/mockServerStore";
+import { getConfig } from "./libs/config";
+import serverStore from "./libs/mockServerStore";
 
 window.config = config;
 window.serverStore = window.serverStore || serverStore;
